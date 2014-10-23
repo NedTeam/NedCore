8c979d626f3167a310e83e3944374df4d680c0b2
GuilleDF <gdefermin@gmail.com>
2014-10-23 19:46:06 +0200

    Updated Layer.java Updated RenderingLayer.java

--- app/src/main/java/com/neddevteam/costumefrenzy/layer/Layer.java
+++ app/src/main/java/com/neddevteam/costumefrenzy/layer/Layer.java
@@ -1,10 +1,16 @@
 package com.neddevteam.costumefrenzy.layer;
 
+import android.graphics.Bitmap;
+
 /**
  * Created by gdefermin on 10/23/14.
  */
 public interface Layer {
 
     public boolean isRenderable();
+
+    public int getPriority();
+
+    public Bitmap getImage();
 
 }
--- app/src/main/java/com/neddevteam/costumefrenzy/layer/RenderingLayer.java
+++ app/src/main/java/com/neddevteam/costumefrenzy/layer/RenderingLayer.java
@@ -1,11 +1,47 @@
 package com.neddevteam.costumefrenzy.layer;
 
+import android.graphics.Bitmap;
+
 /**
  * Created by gdefermin on 10/23/14.
  */
 public class RenderingLayer implements Layer {
+    private int imageResourceID;
+    private int dataResourceID;
+    private boolean dynamic;
+
+    public RenderingLayer(int imageResourceID){
+        this.imageResourceID = imageResourceID;
+        parse();
+    }
+
+    public RenderingLayer(int imageResourceID, int dataResourceID){
+        this.imageResourceID = imageResourceID;
+        this.dataResourceID = dataResourceID;
+    }
+
     @Override
     public boolean isRenderable() {
         return true;
+    }
+
+    @Override
+    public int getPriority() {
+        //Stub
+        return 0;
+    }
+
+    @Override
+    public Bitmap getImage() {
+        return null;
+    }
+
+
+    private void parse() {
+
+    }
+
+    public boolean isDynamic() {
+        return dynamic;
     }
 }
