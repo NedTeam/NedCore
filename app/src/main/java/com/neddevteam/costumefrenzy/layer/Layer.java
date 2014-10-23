ab95d7a97cd70f7e63f3efc18cca5d0fc9871c60
GuilleDF <gdefermin@gmail.com>
2014-10-23 18:56:50 +0200

    Created Layer.java Created RenderingLayer.java

---
+++ app/src/main/java/com/neddevteam/costumefrenzy/layer/Layer.java
@@ -0,0 +1,10 @@
+package com.neddevteam.costumefrenzy.layer;
+
+/**
+ * Created by gdefermin on 10/23/14.
+ */
+public interface Layer {
+
+    public boolean isRenderable();
+
+}
---
+++ app/src/main/java/com/neddevteam/costumefrenzy/layer/RenderingLayer.java
@@ -0,0 +1,11 @@
+package com.neddevteam.costumefrenzy.layer;
+
+/**
+ * Created by gdefermin on 10/23/14.
+ */
+public class RenderingLayer implements Layer {
+    @Override
+    public boolean isRenderable() {
+        return true;
+    }
+}
