553484b613ffd055977f03908f9dd65e3aa242b4
GuilleDF <gdefermin@gmail.com>
2015-01-31 19:41:36 +0100

    Started work on event system

---
+++ app/src/main/java/com/neddevteam/costumefrenzy/event/Event.java
@@ -0,0 +1,7 @@
+package com.neddevteam.costumefrenzy.event;
+
+/**
+ * Created by gdefermin on 1/31/15.
+ */
+public interface Event {
+}
---
+++ app/src/main/java/com/neddevteam/costumefrenzy/event/EventManager.java
@@ -0,0 +1,33 @@
+package com.neddevteam.costumefrenzy.event;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by gdefermin on 1/31/15.
+ */
+public class EventManager {
+    private List<Listener> registeredListeners;
+
+    public EventManager() {
+        registeredListeners = new ArrayList<Listener>();
+    }
+
+    public void register(Listener listener) {
+        if(!registeredListeners.contains(listener)) {
+            registeredListeners.add(listener);
+
+            Method[] methods = listener.getClass().getMethods();
+            for(Method method: methods){
+                if (method.getAnnotation(HandleEvent.class) != null){
+                    //TODO
+                }
+            }
+        }
+    }
+
+    public void callEvent(Event event){
+        //TODO
+    }
+}
