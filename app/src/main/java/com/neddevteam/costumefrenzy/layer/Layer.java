d0f11921297f5e3830a425cb59817806369b4264
mcat95 <mcat95@gmail.com>
2015-02-07 18:17:42 +0100

    Clearing code, and start rewriting rendering core

--- app/src/main/java/com/neddevteam/costumefrenzy/layer/Layer.java
+++ app/src/main/java/com/neddevteam/costumefrenzy/layer/Layer.java
@@ -1,16 +1,12 @@
 package com.neddevteam.costumefrenzy.layer;
 
-import android.graphics.Bitmap;
-
 /**
  * Created by gdefermin on 10/23/14.
  */
 public interface Layer {
 
-    public boolean isRenderable();
+    public boolean isDrawable();
 
     public int getPriority();
-
-    public Bitmap getImage();
 
 }
--- app/src/main/java/com/neddevteam/costumefrenzy/layer/RenderingLayer.java
+++ app/src/main/java/com/neddevteam/costumefrenzy/layer/RenderingLayer.java
@@ -1,78 +1,32 @@
 package com.neddevteam.costumefrenzy.layer;
 
-import android.content.res.AssetManager;
-import android.content.res.Resources;
 import android.graphics.Bitmap;
-import android.util.Log;
-
-import com.neddevteam.costumefrenzy.utils.JSONUtils;
-import com.neddevteam.costumefrenzy.utils.RenderingUtils;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 /**
  * Created by gdefermin on 10/23/14.
  */
 public class RenderingLayer implements Layer {
 
-    private Resources res;
-    private int imageResourceID;
-    private String imageResourceName;
-    private String dataResourceName;
-    private AssetManager am;
-
-    public Bitmap getBitmap() {
-        return bitmap;
-    }
-
+    private int priority;
     private Bitmap bitmap;
 
-    private boolean dynamic;
-
-    public RenderingLayer(Resources res,int imageResourceID){
-        this.res = res;
-        this.imageResourceID=imageResourceID;
+    public RenderingLayer(Bitmap bitmap, int priority){
+        this.bitmap = bitmap;
+        this.priority = priority;
     }
-
-    public RenderingLayer(String imageResourceName, String dataResourceName, AssetManager am){
-        this.imageResourceName = imageResourceName;
-        this.dataResourceName = dataResourceName;
-        this.am = am;
-        parse();
-    }
-
 
     @Override
-    public boolean isRenderable() {
+    public boolean isDrawable() {
         return true;
     }
 
     @Override
     public int getPriority() {
-        //Stub
-        return 0;
+        return priority;
     }
 
-    @Override
-    public Bitmap getImage() {
-        return null;
+    public Bitmap getBitmap() {
+        return bitmap;
     }
 
-
-    private void parse() {
-        if(dataResourceName==null)return;
-        try {
-            JSONObject object = new JSONObject(JSONUtils.loadStringFromAsset(am, dataResourceName));
-            Bitmap map = (imageResourceID==0) ? RenderingUtils.parseRender(object, imageResourceName) :
-                    RenderingUtils.parseRender(res,object,imageResourceID);
-            bitmap = map;
-        } catch (JSONException e) {
-            Log.e("CostumeFrenzy",e.getMessage());
-        }
-    }
-
-    public boolean isDynamic() {
-        return dynamic;
-    }
 }
